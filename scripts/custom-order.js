dfd10e730569aa61d6af248258ee74cf32c01772
steff-dz <69957357+steff-dz@users.noreply.github.com>
2020-10-30 20:02:56 +0100

    added hammer animation

--- scripts/custom-order.js
+++ scripts/custom-order.js
@@ -1,3 +1,19 @@
 const main = document.querySelector('#main');
-console.log(nav, main);
 nav.after(main);
+
+const hammer = document.querySelector('.fa-hammer');
+const startButton = document.querySelector('#startButton');
+
+startButton.addEventListener('click', dropHammer);
+
+function dropHammer() {
+	//hammer.style.color = 'red';
+	hammer.style.transform = 'translateX(12px) translateY(5px) rotate(65deg)';
+	hammer.style.transition = '.4s';
+	startButton.style.transitionDelay = '.3s';
+	startButton.style.backgroundColor = 'lightgreen';
+	// if (startButton.style.backgroundColor === 'lightgreen') {
+	// 	main.innerHTML = '';
+	// 	main.innerHTML = `NEXT PAGE WEE!`;
+	// }
+}
